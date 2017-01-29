6b9d7ade294f71f88fea944848e74b509fbd1068
pkadambi <pkadambi1@gmail.com>
2017-01-29 12:51:09 -0700

    Threading works

--- CoughRecorderGUI.py
+++ CoughRecorderGUI.py
@@ -8,10 +8,13 @@ import sys
 import winsound
 import time
 import pyaudio
+import threading
+import time
 from PyQt5 import QtWidgets
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow, QLabel, QLineEdit
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow, QLabel, QLineEdit, QSplitter, QFrame
 from PyQt5.QtCore import pyqtSlot
+from PyQt5.Qt import QHBoxLayout
 
 class CoughRecorderGUI(QWidget):
     
@@ -29,6 +32,27 @@ class CoughRecorderGUI(QWidget):
         self.initUI()          
 
     def initUI(self):
+        
+        
+        hbox = QHBoxLayout(self)
+        
+        #Top, middle, and bottom pannels initialized for Settings, Record, and Status areas
+        topSettingsFrame = QFrame(self)
+        topSettingsFrame.setFrameShape(QFrame.StyledPanel)      
+        
+        middleRecordFrame = QFrame(self)
+        middleRecordFrame.setFrameShape(QFrame.StyledPanel) 
+        
+        bottomStatusFrame = QFrame(self)
+        bottomStatusFrame.setFrameShape(QFrame.StyledPanel) 
+        
+        splitter1 = QSplitter(Qt.Vertical)
+        splitter1.addWidget(topSettingsFrame)
+        splitter1.addWidget(middleRecordFrame)
+        splitter1.addWidget(bottomStatusFrame)
+        
+        self.setLayout(hbox)
+        
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
         
@@ -45,29 +69,64 @@ class CoughRecorderGUI(QWidget):
         self.promptDeviceIDLabel = QLabel('Enter device ID (REQUIRED):',self) 
         self.promptDeviceIDLabel.setFixedWidth(150)
         self.promptDeviceIDLabel.move(30,20) 
-        
+#         self.statusBar()
         recordBtn = QPushButton('Record', self)
         recordBtn.setToolTip('Press this button to begin recording the trial')
         recordBtn.move(30,80) 
-        recordBtn.clicked.connect(self.on_click_record)
+        recordBtn.clicked.connect(self.recordButtonClicked)
         
         stoprecordBtn = QPushButton('STOP', self)
         stoprecordBtn.setToolTip('Press this button to STOP Recording data')
         stoprecordBtn.move(190,80) 
-        stoprecordBtn.clicked.connect(self.on_click_stop)
+        stoprecordBtn.clicked.connect(self.stopRecordingButtonClicked)
         
         self.show()
         
     def testPrint(self):
-        return 'If this message is heard before the label text changes, Fuck Python'
+        print('If this message is printed before the label text changes, you\'re screwed')
     
-    def updateRecordStatus(self,statusStr):
+    #This function is called to update the Label indicating if we are recording
+    def updateRecordStatusLabel(self,statusStr):
         self.recordIndicatorLabel.setText(statusStr)
 
-    def buttonClicked(self):
-        sender = self.sender()
-        pass
+    #def     
+    def recordButtonClicked(self):
+        #1. create new file given device ID
+            #maybe allow to select the folder
+            #1a. check for old files with name, rename
+            #create file and then
         #code for where pressing the C button will record the cough
+        def callback():
+            
+            #Update status label
+            self.updateRecordStatusLabel('Current Status: Recording NOW')
+            
+            #Get the device id
+            self.deviceID = int(self.textboxDevID.text())
+            print(self.deviceID)
+            self.testPrint()
+            
+            #Play the indicator tone
+            time.sleep(1)
+            winsound.Beep( 1000+self.deviceID*100, 1500)
+
+            #Simulate writing to file for now
+            i, sum = 0,0
+            start = time.time()
+            
+            while (i<5000):
+                time.sleep(0.00001) #minimum sleep time will be 1ms
+                sum+=1
+                print(sum)
+                i+=1
+            print(time.time()-start)
+            
+        t = threading.Thread(target = callback)
+        t.start()
+        
+    def stopRecordingButtonClicked(self):
+        self.updateRecordStatusLabel('Current Status: NOT Recording')
+        
 
     def keyPressEvent(self, e):
         
@@ -76,23 +135,23 @@ class CoughRecorderGUI(QWidget):
             
             
             
-#---Problem code-----------------
-    @pyqtSlot()
-    def on_click_record(self):
-        #check if valid device id is given here first
-        self.updateRecordStatus('Current Status: Recording NOW')
-        self.deviceID = int(self.textboxDevID.text())
-        print(self.deviceID)
-        
-        self.testPrint()
-        time.sleep(4)
-        #play the sound
-        #winsound.Beep(1000+self.deviceID*100, 3000)
-        winsound.Beep( 1000+self.deviceID*100, 3000)
-        
-    def on_click_stop(self):
-        self.updateRecordStatus('Current Status: Recording done, Ready for next trial')
-
+#---Problem code----------------- old way of handling events
+#     @pyqtSlot()
+#     def on_click_record(self):
+#         #check if valid device id is given here first
+#         self.updateRecordStatus('Current Status: Recording NOW')
+#         self.deviceID = int(self.textboxDevID.text())
+#         print(self.deviceID)
+#         
+#         self.testPrint()
+#         time.sleep(4)
+#         #play the sound
+#         #winsound.Beep(1000+self.deviceID*100, 3000)
+#         winsound.Beep( 1000+self.deviceID*100, 3000)
+#         
+#     def on_click_stop(self):
+#         self.updateRecordStatus('Current Status: Recording done, Ready for next trial')
+#----- Problem code------
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
---
+++ junk.py
@@ -0,0 +1,11 @@
+'''
+Created on Jan 29, 2017
+
+@author: Prad
+'''
+import time
+start = time.time()
+
+time.sleep(0.000001)
+end = time.time() - start
+print(end)
